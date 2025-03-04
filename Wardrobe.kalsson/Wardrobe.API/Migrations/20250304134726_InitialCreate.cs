f47edd8c38c1af6d0b3a1e16f7feac80ac7ca63b
jklogi <jk@logimark.se>
2025-03-04 14:48:51 +0100

    Add initial migration and configure SQLite database
    Implemented the first database migration to create the "WardrobeItems" table with essential fields. Configured the application to use SQLite as the database provider with a corresponding DbContext setup.

---
+++ Wardrobe.kalsson/Wardrobe.API/Migrations/20250304134726_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wardrobe.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WardrobeItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    ImagePath = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WardrobeItems", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WardrobeItems");
+        }
+    }
+}
