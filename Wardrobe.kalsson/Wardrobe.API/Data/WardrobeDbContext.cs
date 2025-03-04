2665d620baa399c947b42855d013aab77e2ef3e5
jklogi <jk@logimark.se>
2025-03-04 14:44:21 +0100

    Add EF Core and initial WardrobeItem model with DbContext
    Integrated Entity Framework Core packages for data handling and created the `WardrobeItem` model to represent wardrobe items. Implemented `WardrobeDbContext` to manage database context and the `WardrobeItems` DbSet for future database operations.

---
+++ Wardrobe.kalsson/Wardrobe.API/Data/WardrobeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wardrobe.API.Models;
+
+namespace Wardrobe.API.Data;
+
+public class WardrobeDbContext : DbContext
+{
+    public WardrobeDbContext(DbContextOptions<WardrobeDbContext> options)
+    : base(options)
+    {
+        
+    }
+
+    public DbSet<WardrobeItem> WardrobeItems { get; set; }
+}
