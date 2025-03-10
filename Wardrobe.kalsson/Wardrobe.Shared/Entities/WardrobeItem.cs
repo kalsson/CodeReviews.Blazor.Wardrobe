5ef2d5e2416ef87a0bfd057095a723170bd015aa
jklogi <jk@logimark.se>
2025-03-10 08:58:56 +0100

    Remove WeatherForecast API and add WardrobeItem entity.
    The WeatherForecast API and related structures were removed as they are no longer needed. Introduced a new `WardrobeItem` entity to the shared project for better alignment with the application's domain. Updated project files to reflect these changes and ensure proper organization.

---
+++ Wardrobe.kalsson/Wardrobe.Shared/Entities/WardrobeItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Wardrobe.Shared.Entities;
+
+public class WardrobeItem
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public string? ImagePath { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public DateTime? DateUpdated { get; set; }
+}
