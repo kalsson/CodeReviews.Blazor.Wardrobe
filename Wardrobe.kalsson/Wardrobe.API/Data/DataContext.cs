e10d09e1916ca44898b894e81cff21c491794654
jklogi <jk@logimark.se>
2025-03-10 16:19:28 +0100

    Add DataContext class for EF Core integration
    Implemented a `DataContext` class with a DbSet for `WardrobeItem`, enabling Entity Framework Core support within the API. This allows seamless interaction with the database and sets the foundation for data access in the application.

---
+++ Wardrobe.kalsson/Wardrobe.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wardrobe.Shared.Entities;
+
+namespace Wardrobe.API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<WardrobeItem> WardrobeItems { get; set; }
+}
